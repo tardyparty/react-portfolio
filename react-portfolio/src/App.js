09fe1cc5b0c0f97e3cb12a6e05e740a7dad31555
tardyparty <tardyparty@users.noreply.github.com>
2020-02-26 23:07:01 -0600

    project div allows scroll and links updated

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import './app.css';
 
-// import Sidebar from "./components/sidebar";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFile } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin, faGithubSquare } from "@fortawesome/free-brands-svg-icons";
+
 
 function App() {
     return (
@@ -11,23 +14,21 @@ function App() {
             <div className="linkicon">
                 <a href="https://www.linkedin.com/in/davidtardy/"
                       target="_blank"> 
-                  <i className="fab fa-linkedin">
-                  </i>
+                  <FontAwesomeIcon icon={faLinkedin} size="3x"/>
                   <h3> linkedin </h3>
                 </a>
               </div>
               <div className="linkicon">
                 <a href="https://github.com/tardyparty"
                   target="_blank">
-                  <i class="fab fa-github-square"></i>
+                  <FontAwesomeIcon icon={faGithubSquare} size="3x"/>
                   <h3> github </h3>
                 </a>
               </div>
               <div className="linkicon">
                 <a href="https://docs.google.com/document/d/e/2PACX-1vTEx96y-lm3S0Xy5I7NuxJSDUuZOI26-ld1ESDcA8eOtKTmE3acX6eP-oSnWt29ltDIyIcDMA4zNO8J/pub"
                   target="_blank">
-                  <i class="far fa-file">
-                  </i>
+                  <FontAwesomeIcon icon={faFile} size="3x"/>
                   <h3> resume </h3>
                 </a>
               </div>
@@ -36,10 +37,16 @@ function App() {
         <div className="main">
           <div className="projectdiv">
             <div className="project">
-
+              Book Club
             </div>
             <div className="project">
-              
+              Happy Camper
+            </div>
+            <div className="project">
+              Clicky Game
+            </div>
+            <div className="project">
+              MedSched
             </div>
           </div>
         </div>
@@ -49,31 +56,31 @@ function App() {
             <p>
               <em className="blue title">fullstack</em>.<em className="green title">developer</em>
               <em className="pink title">()</em> <em className="yellow title"> {"{"} </em> <br></br>
-              focused on developing new <br></br>
-              skills in challenging environments. <br></br>
+              &emsp;focused on developing new <br></br>
+              &emsp;skills in challenging environments. <br></br>
               <em className="yellow title"> {"}"} </em> ;
             </p>
             <p>
               <em className="pink"> class </em> <em className="blue"> TechnicalSkills </em>
               = <em className="green"> function</em> <em className="pink">() </em> 
               <em className="yellow">{"{"}</em> <br></br>
-              <em className="green">return </em> <em className="pink">(</em><br></br>
-                <em className="orange">languages</em> : <em className="purple"> [ </em><br></br>
-              Javascript, <br></br>
-              HTML / CSS, <br></br>
-              Python <br></br>
-              <em className="purple">]</em> , <br></br>
-              <em className="orange">frameworks</em> : <em className="purple"> [ </em><br></br>
-              React, <br></br>
-              Node, <br></br>
-              Express, <br></br>
-              Mongoose <br></br>
-              <em className="purple">]</em> , <br></br>
-              <em className="orange">databases</em> : <em className="purple"> [ </em><br></br>
-              MongoDB, <br></br>
-              MySQL <br></br>
-              <em className="purple">]</em><br></br>
-              <em className="purple">)</em><br></br>
+              &emsp;<em className="green">return </em> <em className="pink">(</em><br></br>
+              &emsp;&emsp;<em className="orange">languages</em> : <em className="purple"> [ </em><br></br>
+              &emsp;&emsp;&emsp;Javascript, <br></br>
+              &emsp;&emsp;&emsp;HTML / CSS, <br></br>
+              &emsp;&emsp;&emsp;Python <br></br>
+              &emsp;&emsp;<em className="purple">]</em> , <br></br>
+              &emsp;&emsp;<em className="orange">frameworks</em> : <em className="purple"> [ </em><br></br>
+              &emsp;&emsp;&emsp;React, <br></br>
+              &emsp;&emsp;&emsp;Node, <br></br>
+              &emsp;&emsp;&emsp;Express, <br></br>
+              &emsp;&emsp;&emsp;Mongoose <br></br>
+              &emsp;&emsp;<em className="purple">]</em> , <br></br>
+              &emsp;&emsp;<em className="orange">databases</em> : <em className="purple"> [ </em><br></br>
+              &emsp;&emsp;&emsp;MongoDB, <br></br>
+              &emsp;&emsp;&emsp;MySQL <br></br>
+              &emsp;&emsp;<em className="purple">]</em><br></br>
+              &emsp;<em className="purple">)</em><br></br>
               <em className="yellow">{"}"}</em>;
             </p>
           </div>
