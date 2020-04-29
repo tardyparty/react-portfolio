f262f9d9a73dcb2ebeebfa26befceaaf9a5bf24b
tardyparty <tardyparty@users.noreply.github.com>
2020-04-29 15:37:47 -0500

    updated small details

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -58,29 +58,49 @@ function App() {
         <div className="main">
           <div className="projectdiv">
             <div className="projectsTitle text-center">Projects</div>
-            <div className="project">
-              Book Club
-              <div className="projectLinks">
-                <a href="" target="_blank" className="linkClass">
-                  <p><em className="yellow">{"{ "}</em><em className="blue">project</em><em className="yellow">{" }"}</em></p>
-                </a>
-                <a href="https://github.com/tardyparty/bookClubApp" 
-                  target="_blank"
-                  className="linkClass">
-                  <p><em className="yellow">{"{ "}</em><em className="green">github</em><em className="yellow">{" }"}</em></p>
-                </a>
+              <div className="project">
+                Happy Camper
+                <div className="projectLinks">
+                  <a href="http://happycamperapp.herokuapp.com/home"
+                    target="_blank" 
+                    className="linkClass">
+                    <p className="linkP"><em className="yellow">{"{ "}</em><em className="blue">project</em><em className="yellow">{" }"}</em></p>
+                  </a>
+                  <a href="https://github.com/rbartl2/final-project" 
+                    target="_blank"
+                    className="linkClass">
+                    <p><em className="yellow">{"{ "}</em><em className="green">github</em><em className="yellow">{" }"}</em></p>
+                  </a>
+                </div>
               </div>
+              <div className="project">
+                Clicky Game
+                <div className="projectLinks">
+                  <a href="https://tardyparty.github.io/clickyGame/" target="_blank" className="linkClass">
+                    <p><em className="yellow">{"{ "}</em><em className="blue">project</em><em className="yellow">{" }"}</em></p>
+                  </a>
+                  <a href="https://github.com/tardyparty/clickyGame"
+                    target="_blank"
+                    className="linkClass">
+                    <p><em className="yellow">{"{ "}</em><em className="green">github</em><em className="yellow">{" }"}</em></p>
+                  </a>
+                </div>
+              </div>
+              <div className="project">
+                Google Books
+                <div className="projectLinks">
+                  <a href="https://floating-bayou-94316.herokuapp.com/mybooks" target="_blank" className="linkClass">
+                    <p><em className="yellow">{"{ "}</em><em className="blue">project</em><em className="yellow">{" }"}</em></p>
+                  </a>
+                  <a href="https://github.com/tardyparty/gBooks"
+                    target="_blank"
+                    className="linkClass">
+                    <p><em className="yellow">{"{ "}</em><em className="green">github</em><em className="yellow">{" }"}</em></p>
+                  </a>
+                </div>
+              </div>
+
             </div>
-            <div className="project">
-              Happy Camper
-            </div>
-            <div className="project">
-              Clicky Game
-            </div>
-            <div className="project">
-              MedSched
-            </div>
-          </div>
         </div>
         <div className="bio">
           <div className="bioFunction">
