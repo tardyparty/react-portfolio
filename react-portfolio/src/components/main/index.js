ef05b81489fc345a6f391cf0f8a67da9b0712407
tardyparty <tardyparty@users.noreply.github.com>
2020-05-01 15:21:30 -0500

    css and copy finished

--- react-portfolio/src/components/main/index.js
+++ react-portfolio/src/components/main/index.js
@@ -7,22 +7,64 @@ const Main = function() {
           <div className="projectdiv">
             <div className="projectsTitle text-center">Projects</div>
               <div className="project">
-                Happy Camper
-                <div className="projectLinks">
-                  <a href="http://happycamperapp.herokuapp.com/home"
+                <div className="thisProjectTitle">
+                  Happy Camper
+                </div>
+                <div className="projectContent">
+                  <div className="smallContent">
+                    <p className="smallTitle">
+                      About:
+                    </p>
+                    <p className="about">
+                      Happy Camper is a gear rental site for beginner campers. Gear packages are put together based on their needs, as well as the ability to ask questions in a forum and search for campsites nationwide.
+                    </p>
+                    <div className="divider"></div>
+                    <a href="http://happycamperapp.herokuapp.com/home"
                     target="_blank" 
                     className="linkClass">
                     <p className="linkP"><em className="yellow">{"{ "}</em><em className="blue">project</em><em className="yellow">{" }"}</em></p>
                   </a>
-                  <a href="https://github.com/rbartl2/final-project" 
+                  </div>
+                  <div className="smallContent">
+                    <p className="smallTitle">
+                      Tech:
+                    </p>
+                    <p className="about">
+                      <strong>Frontend:</strong> React, Redux, Bootstrap <br></br>
+                      <strong>Backend:</strong> Node, Mongo, Passport, JSon Web Tokens <br></br>
+                      <strong>My Role:</strong> Entirety of Backend, Frontend Framework, Authorization, Redux
+                    </p>
+                    <div className="divider"></div>
+                    <a href="https://github.com/rbartl2/final-project" 
                     target="_blank"
                     className="linkClass">
                     <p><em className="yellow">{"{ "}</em><em className="green">github</em><em className="yellow">{" }"}</em></p>
                   </a>
+                  </div>
                 </div>
               </div>
-              <div className="project">
-                Clicky Game
+              {/* <div className="project">
+                <div className="thisProjectTitle">
+                  Clicky Game
+                </div>
+                <div className="projectContent">
+                  <div className="smallContent">
+                    <p className="smallTitle">
+                      About:
+                    </p>
+                    <p className="about">
+                      Clicky Game is a simple memory Game built using React.
+                    </p>
+                  </div>
+                  <div className="smallContent">
+                    <p className="smallTitle">
+                      Tech:
+                    </p>
+                    <p className="about">
+                      React
+                    </p>
+                  </div>
+                </div>
                 <div className="projectLinks">
                   <a href="https://tardyparty.github.io/clickyGame/" target="_blank" className="linkClass">
                     <p><em className="yellow">{"{ "}</em><em className="blue">project</em><em className="yellow">{" }"}</em></p>
@@ -33,21 +75,42 @@ const Main = function() {
                     <p><em className="yellow">{"{ "}</em><em className="green">github</em><em className="yellow">{" }"}</em></p>
                   </a>
                 </div>
-              </div>
+              </div> */}
               <div className="project">
+                <div className="thisProjectTitle">
                 Google Books
-                <div className="projectLinks">
-                  <a href="https://floating-bayou-94316.herokuapp.com/mybooks" target="_blank" className="linkClass">
+                </div>
+                <div className="projectContent">
+                  <div className="smallContent">
+                    <p className="smallTitle">
+                      About:
+                    </p>
+                    <p className="about">
+                      Google Books is a fullstack app that allows users to search the Google Books API and save their favorite books to a List where they can see all of their books.
+                    </p>
+                    <div className="divider"></div>
+                    <a href="https://floating-bayou-94316.herokuapp.com/mybooks" target="_blank" className="linkClass">
                     <p><em className="yellow">{"{ "}</em><em className="blue">project</em><em className="yellow">{" }"}</em></p>
-                  </a>
-                  <a href="https://github.com/tardyparty/gBooks"
+                    </a>
+                  </div>
+                  <div className="smallContent">
+                    <p className="smallTitle">
+                      Tech:
+                    </p>
+                    <p className="about">
+                      <strong>Frontend:</strong> React, Bootstrap, Google Books API <br></br>
+                      <strong>Backend:</strong> Node, Mongo, Mongoose <br></br>
+                      <strong>My Role:</strong> Built everything
+                    </p>
+                    <div className="divider"></div>
+                    <a href="https://github.com/tardyparty/gBooks"
                     target="_blank"
                     className="linkClass">
                     <p><em className="yellow">{"{ "}</em><em className="green">github</em><em className="yellow">{" }"}</em></p>
                   </a>
+                  </div>
                 </div>
               </div>
-
             </div>
         </div>
     )
